fc36555bfb154b146efd9bd75a8534fa7ab0fc1b
FFeibanaqi <985742146@qq.com>
2017-12-09 14:03:49 +0800

    update framework to version2

---
+++ EAS_v2/src/com/eas/model/quiz_system/Readme.java
@@ -0,0 +1,9 @@
+package com.eas.model.quiz_system;
+
+public class Readme {
+
+	public static void main(String[] args) {
+		//占位用的java文件，在自己的package包创文件后删除
+	}
+
+}
---
+++ EAS_v2/src/com/eas/service/Readme4.java
@@ -0,0 +1,9 @@
+package com.eas.service;
+
+public class Readme4 {
+
+	public static void main(String[] args) {
+		//占位用的java文件，在自己的package包创文件后删除
+	}
+
+}
