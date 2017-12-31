3b61b9f73fde88d7455696fd1c096e321ab15351
LiMolly <2393755529@qq.com>
2017-12-31 21:39:08 +0800

    增加作业评价表结构

--- EAS_v2/WebRoot/eas_ext.sql
+++ EAS_v2/WebRoot/eas_ext.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS `rollcall`;
  CREATE TABLE `rollcall` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `C_IDS` varchar(255) DEFAULT NULL,
@@ -6,4 +7,15 @@
   `expired_at` datetime(6) DEFAULT NULL,
   `T_ID` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `homework_comment`;
+CREATE TABLE `homework_comment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` varchar(255) DEFAULT NULL,
+  `created_at` datetime(6) DEFAULT NULL,
+  `S_ID` varchar(255) DEFAULT NULL,
+  `T_ID` varchar(255) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
